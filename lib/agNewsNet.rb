0bcbc0d71d1a792bb2cb8ed55f8a016c1c30c72a
Ekaterina0801 <ds.katrin@mail.ru>
2022-10-26 19:26:13 +0300

    Create neural network

--- lib/agNewsNet.rb
+++ lib/agNewsNet.rb
@@ -1,1 +1,19 @@
+require ''
+module AGNews
+  module Net
+    class << self
+      attr_writer :batch_size, :count_epochs
+      def trainWithNewParameters(batch_size, count_epochs)
+        @batch_size = batch_size
+        @count_epochs = count_epochs
 
+      end
+    end
+  end
+  module Classifier
+    class <<self
+      attr_accessor :file_path
+
+    end
+  end
+end
